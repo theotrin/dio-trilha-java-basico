8b8321c53bff9d1dce4e9e5506b57d7815a44a2a
theotrin <theopinheiro12@gmail.com>
2024-04-24 15:22:22 -0400

    projeto finalizado!

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,21 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+System.out.println("Olá qual seu nome?");
+String nome = System.console().readLine();
+
+System.out.println("E qual sua agencia?");
+String agencia = System.console().readLine();
+
+System.out.println("Qual nuemro da sua conta?");
+String numero = System.console().readLine();
+
+System.out.println("Qual seu saldo?");
+String saldo = System.console().readLine();
+
+
+
+System.out.println("Olá "+ nome + ", obrigado por criar uma conta em nosso banco, sua agência é " +agencia+", conta "+numero+" e seu saldo "+saldo+ " já está disponível para saque!");  
+
+}
 }
